14ead7a4d94ff7fc67a8615a263ad04ae3030262
Chris Cummer <chris@unbounce.com>
2013-02-04 14:48:08 -0800

    All specs passing

--- test/lib/area_codes/area_code_list_test.rb
+++ test/lib/area_codes/area_code_list_test.rb
@@ -3,21 +3,26 @@ require_relative '../../test_helper'
 class AreaCodeListTest < Test::Unit::TestCase
 
   def setup
-    @area_code_list = AreaCodes::AreaCodeList.new('canada.txt')
+    @single = 'canada.txt'
+    @multiple = ['canada.txt', 'mexico.txt']
+  end
+
+  def list(file_names)
+    AreaCodes::AreaCodeList.new(file_names)
   end
 
   def test_initialize
-    assert_equal( true, @area_code_list.area_codes.instance_of?(Set) )
-    assert_equal( false, @area_code_list.area_codes.empty? )
+    assert_equal( true, list(@single).area_codes.instance_of?(Set) )
+    assert_equal( false, list(@single).area_codes.empty? )
   end
 
   def test_include?
-    assert_equal( true, @area_code_list.include?(604) )
-    assert_equal( false, @area_code_list.include?(666) )
+    assert_equal( true, list(@single).include?(604) )
+    assert_equal( false, list(@single).include?(666) )
   end
 
   def test_exclude?
-    assert_equal( false, @area_code_list.exclude?(604) )
-    assert_equal( true, @area_code_list.exclude?(666) )
+    assert_equal( false, list(@single).exclude?(604) )
+    assert_equal( true, list(@single).exclude?(666) )
   end
 end
