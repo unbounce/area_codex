919a7db261e29c751bf8092da7849b050452c8de
Chris Cummer <chris@unbounce.com>
2013-02-05 11:18:01 -0800

    Trying to remove the need for users to require Forwardable

--- lib/area_codex/concerns/with_area_codes.rb
+++ lib/area_codex/concerns/with_area_codes.rb
@@ -1,6 +1,7 @@
 module AreaCodex
   module WithAreaCodes
-    extend ::Forwardable
+    require 'forwardable'
+    extend Forwardable
 
     def_delegator :@area_code_list, :include?, :include?
     def_delegator :@area_code_list, :exclude?, :exclude?
